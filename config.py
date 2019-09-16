3242f3508c203a770b9c4fb1d46e311cbdf26ca7
usanase Francoise <fusanasencoise@gmail.com>
2019-09-16 11:37:49 +0200

    add procfile

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
-    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_API_KEY ='7898c1e92e3f4963b15bf09d8dcb0c15'
 
     @staticmethod
     def init_app(app):
